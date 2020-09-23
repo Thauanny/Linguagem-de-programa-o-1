ac4df848be1379445b042aaac5d51b239aeec9fa
thauanny <thauannyk@ufrn.edu.br>
2020-09-23 18:36:27 -0300

    conclusao de destrutores e inicio de estatico e random

---
+++ Encapsulamento/dado.hpp
@@ -0,0 +1,24 @@
+#ifndef _DADO_H_
+#define _DADO_H_
+
+
+class Dado{
+private:
+	int valor;
+	std::random_devide rd;
+	std::default_random_engine gen;
+	std::uniform_distribution<> dist;
+
+
+
+	Dado();
+	
+}
+
+
+
+
+
+
+
+#endif
---
+++ Encapsulamento/dado_main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "dado.hpp"
+
+int main(int argc, char const *argv[]){
+	
+	if(argc == 2){
+		Dado meu_dado;
+		for(int i = 0; <atoi()){
+
+		}
+	}
+
+
+	return 0;
+}
